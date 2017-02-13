2d99e5ad2f0689e7e3bb74987a5d2e11c8f12c16
André Matias <dev.andrematias@gmail.com>
2017-02-13 15:29:24 -0200

    Initialing the application with index and configs initials

---
+++ app/config/configs.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS['paths'] = array(
+	'root' => __DIR__.'/../../',
+	'media'=> __DIR__.'/../../media/',
+	'bin'  => __DIR__.'/../../bin',
+	'controllers' => __DIR__.'/../bundles/CoreBundle/Controllers/'
+);
+
+
+require_once($GLOBALS['paths']['root'].'vendor/autoload.php');
