0f5382d9eb366ce5a37e53847a1d3fa1f5328fa0
André Matias <dev.andrematias@gmail.com>
2017-02-14 19:13:10 -0200

    Add new configurations for global scope

--- app/config/configs.php
+++ app/config/configs.php
@@ -7,5 +7,14 @@ $GLOBALS['paths'] = array(
 	'controllers' => __DIR__.'/../bundles/CoreBundle/Controllers/'
 );
 
+$protocol = (isset($_SERVER['HTTPS'])) ? 'https://' : 'http://';
+$serverName = $_SERVER['SERVER_NAME'];
+$serverPort = (isset($_SERVER['SERVER_PORT'])) ? ':'.$_SERVER['SERVER_PORT'] : '';
+$GLOBALS['site_paths'] = array(
+	'hostname' => $protocol.$serverName.$serverPort
+);
+
+$GLOBALS['site_paths']['media'] = $GLOBALS['site_paths']['hostname'].'/media/';
+
 
 require_once($GLOBALS['paths']['root'].'vendor/autoload.php');
